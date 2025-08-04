daec4f17ba5e52a66b4a59a1374a30102a3937b9
farkenthegreat <numberone@farken.co.uk>
2025-08-04 19:46:25 +0100

    added functions to stats.py

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 from stats import get_word_count
+from stats import count_characters
+from stats import prepare_dict
 
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
 
 def main():
+    print('============ BOOKBOT ============')
     book = get_book_text('./books/frankenstein.txt')
     num_words = get_word_count(book)
-    print(f'{num_words} words found in the document')
-
+    print('----------- Word Count ----------')
+    print(f'Found {num_words} total words')
+    print('--------- Character Count -------')
+    c = count_characters(book)
+    sorted_dict = prepare_dict(c)
+    for thing in sorted_dict:
+        if thing['character'].isalpha():
+            print(thing['character'] + ': ' + str(thing['num']))
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,22 @@
 def get_word_count(book):
     list_of_words = book.split()
     return len(list_of_words)
+
+def count_characters(text):
+    results = dict()
+    for char in text.lower():
+        if char in results:
+            results[char] = results[char] + 1
+        else: 
+            results[char] = 1
+    return results
+
+def sort_on(items):
+    return items["num"]
+
+def prepare_dict(input_dict):
+    list_of_dicts = list()
+    for entry in input_dict.keys():
+        list_of_dicts.append({'character':entry, 'num':input_dict[entry]})
+    list_of_dicts.sort(reverse=True, key=sort_on)
+    return list_of_dicts
