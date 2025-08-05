99e711bf867b94dabf9c78e838cb5ec5f6c1fd7a
farkenthegreat <numberone@farken.co.uk>
2025-08-05 19:31:47 +0100

    path to book now entered via command line

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from stats import get_word_count
 from stats import count_characters
 from stats import prepare_dict
+import sys
 
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
 
 def main():
+    if len(sys.argv) != 2:
+        print('Usage: python3 main.py <path_to_book>')
+        sys.exit(1)
     print('============ BOOKBOT ============')
-    book = get_book_text('./books/frankenstein.txt')
+    book = get_book_text(sys.argv[1])
     num_words = get_word_count(book)
     print('----------- Word Count ----------')
     print(f'Found {num_words} total words')
